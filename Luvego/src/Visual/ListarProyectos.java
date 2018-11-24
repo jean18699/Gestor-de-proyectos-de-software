e75de57fab9d4e3c1428bae30d541874f4a4f6a5
hardow <hardow@DESKTOP-2PCFI88>
2018-11-24 12:01:07 -0400

    Creacion del modelo basico de la ventana ListarProyectos

---
+++ Luvego/src/Visual/ListarProyectos.java
@@ -0,0 +1,134 @@
+package Visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.ScrollPane;
+import java.awt.event.MouseAdapter;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import com.sun.glass.events.MouseEvent;
+
+import Logico.Empresa;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ListarProyectos extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JScrollPane scrollPane;
+	private static JTable table;
+	private int index;
+	private JButton btnDetalles;
+	private static DefaultTableModel model;
+	private static Object[] fila;
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			ListarProyectos dialog = new ListarProyectos();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public ListarProyectos() {
+		setTitle("Lista de proyectos");
+		setBounds(100, 100, 649, 300);
+		setLocationRelativeTo(null);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setLayout(new BorderLayout(0,0));
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		contentPanel.add(scrollPane, BorderLayout.CENTER);
+		{
+			table = new JTable();
+			table.setRowHeight(25);
+			table.addMouseListener(new MouseAdapter() {
+				public void mouseClicked(MouseEvent e) {
+					index = table.getSelectedRow();
+					if(index >= 0) {
+						btnDetalles.setEnabled(true);
+					}
+				}
+			});
+			table.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			table.getTableHeader().setFont(new Font("Tahoma", Font.ITALIC, 18));
+			
+			table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			model = new DefaultTableModel();
+			String[] columnnames = {"Código", "Nombre", "Jefe de Proyecto"};
+			model.setColumnIdentifiers(columnnames);
+			table.setModel(model);
+			scrollPane.setViewportView(table);
+		}
+		
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				btnDetalles = new JButton("Detalles");
+				btnDetalles.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+					}
+				});
+				btnDetalles.setEnabled(false);
+				btnDetalles.setActionCommand("OK");
+				buttonPane.add(btnDetalles);
+				getRootPane().setDefaultButton(btnDetalles);
+			}
+			{
+				JButton btnSalir = new JButton("Salir");
+				btnSalir.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				btnSalir.setActionCommand("Cancel");
+				buttonPane.add(btnSalir);
+			}
+		}
+		cargarProyectos();
+	}
+	
+	private void cargarProyectos() {
+		model.setRowCount(0);
+		fila = new Object[model.getColumnCount()];
+		for(int i = 0; i < Empresa.getInstance().getProyectos().size(); i++) {
+			fila[0] = Empresa.getInstance().getProyectos().get(i).getId();
+			fila[1] = Empresa.getInstance().getProyectos().get(i).getNombre();
+			fila[2] = Empresa.getInstance().getProyectos().get(i).getJefeProyecto().getApellidos();
+			
+			model.addRow(fila);
+		}
+		
+		table.getColumnModel().getColumn(0).setMinWidth(200);
+		table.getColumnModel().getColumn(0).setMaxWidth(200);
+		
+		table.getColumnModel().getColumn(1).setMinWidth(200);
+		table.getColumnModel().getColumn(1).setMaxWidth(200);
+	}
+
+}
